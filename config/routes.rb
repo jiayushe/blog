2b08822176660e579126ff121a70077963496025
zephyr <shejiayu1998@qq.com>
2019-01-28 18:53:09 +0800

    Updated on 28 Jan 2019

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   
-  resources :articles
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles do
+    resources :comments
+  end
+
   root 'welcome#index'
 end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
